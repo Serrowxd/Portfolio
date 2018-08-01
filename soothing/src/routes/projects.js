ab3d4d796a5d65d9ad351f51de62ddf1293b8133
Kevin Jolley <serrowpls@gmail.com>
2018-08-01 03:03:34 -0500

    projects & imgs

--- soothing/src/routes/projects.js
+++ soothing/src/routes/projects.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import img from '../assets/manlaw.png';
+import img2 from '../assets/job.png';
+import img3 from '../assets/github.png';
+
 const Projects = () => {
+  const button = <button class="ripple"> Code </button>;
   return (
     <div className="main_div projects">
-      <h1> Projects </h1>
-      <p> I did stuff (OvO") </p>
+      <h1> I will populate this with my projects as they become available! </h1>
+      <div className="proj_row">
+        <div className="proj_col">
+          <img src={img} class="proj_img" alt="manlaw" />
+          <h4> Mandracchia Law - Freelance </h4>
+          {button}
+        </div>
+        <div className="proj_col">
+          <img src={img2} class="proj_img" alt="manlaw" />
+          <h4> JobMe - Capstone Project </h4>
+          {button}
+        </div>
+        <div className="proj_col">
+          <img src={img3} class="proj_img" alt="manlaw" />
+          <h4> Github </h4>
+          {button}
+        </div>
+      </div>
     </div>
   );
 };
