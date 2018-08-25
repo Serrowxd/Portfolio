3e55346eca5ec5938c3b500cbc42fed45f574ca9
Kevin Jolley <serrowpls@gmail.com>
2018-08-25 07:14:57 -0500

    playing with some styles

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -3,10 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="main_div contact">
-      <h1> Contact </h1>
-      <p>
-        <code> Still a work in progress, refer to my resume instead!</code>
-      </p>
+      <h1> Still working on this! </h1>
     </div>
   );
 };
