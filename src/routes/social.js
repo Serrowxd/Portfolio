ab7c7f10cc0a85a886656bc295cef7ca3f8f4380
Kevin Jolley <serrowpls@gmail.com>
2018-10-06 16:21:23 -0500

    updated

--- src/routes/social.js
+++ src/routes/social.js
@@ -5,52 +5,70 @@ const Social = () => {
     <div className="main_div social">
       <div className="soc_row">
         <div className="soc_col">
-          <h1> Social Links </h1>
-          <a
-            href="https://github.com/Serrowxd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> GitHub </h4>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/serrowxd/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> LinkedIn </h4>
-          </a>
-          <a
-            href="https://app.codesignal.com/profile/serrowxd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> CodeSignal </h4>
-          </a>
+          <h1> Social </h1>
+
+          <h4>
+            <a
+              href="https://github.com/Serrowxd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </h4>
+
+          <h4>
+            <a
+              href="https://www.linkedin.com/in/serrowxd/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </h4>
+
+          <h4>
+            <a
+              href="https://app.codesignal.com/profile/serrowxd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CodeSignal
+            </a>
+          </h4>
         </div>
         <div className="soc_col">
-          <h1> Other Links </h1>
-          <a
-            href="https://www.twitch.tv/serrowpls"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> Twitch </h4>
-          </a>
-          <a
-            href="https://twitter.com/serrowxd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> Twitter </h4>
-          </a>
-          <a
-            href="https://www.instagram.com/serrowxd/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4> Instagram </h4>
-          </a>
+          <h1> Media </h1>
+
+          <h4>
+            <a
+              href="https://www.twitch.tv/serrowpls"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitch
+            </a>
+          </h4>
+
+          <h4>
+            <a
+              href="https://twitter.com/serrowxd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </h4>
+
+          <h4>
+            <a
+              href="https://www.instagram.com/serrowxd/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </h4>
         </div>
       </div>
     </div>
